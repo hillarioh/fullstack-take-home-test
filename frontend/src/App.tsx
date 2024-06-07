ffd9c251a6a194d8ece0f08685566f5b18b278db
hillarioh <okerioh@gmail.com>
2024-06-07 15:52:57 +0300

    setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SearchInput from "./components/SearchInput";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SearchInput />
+      </header>
+    </div>
+  );
+}
+
+export default App;
